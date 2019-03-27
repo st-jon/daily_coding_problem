38b2a37b9d7b5e56b882c58be047b1521a44c377
christophe <c.johanny@gmail.com>
2019-03-27 11:34:40 +0100

    problem 5 done

---
+++ prob_5.js
@@ -0,0 +1,27 @@
+// cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+// For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+// Given this implementation of cons:
+
+// def cons(a, b):
+//     def pair(f):
+//         return f(a, b)
+//     return pair
+// Implement car and cdr.
+
+const cons = (a, b) => {
+    function pair(fn) {
+        return fn(a, b)
+    }
+    return pair
+}
+
+const car = (fn) => {
+    return fn((a,b) => a)
+}
+const cdr = (fn) => {
+    return fn((a,b) => b)
+}
+
+console.log(car(cons(3, 4)))
+console.log(cdr(cons(3, 4)))
